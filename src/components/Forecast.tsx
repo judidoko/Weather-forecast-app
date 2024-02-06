fcc17350b192fa05cac20a0315732637bf9a5b76
Jude Idoko <judidoko@gmail.com>
2024-02-06 13:00:36 +0100

    add reset button to refresh page to first page after api call

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -25,27 +25,40 @@ const Forecast = ({ forecast }: Props): JSX.Element => {
   //   day temp
   const today = forecast.list[0];
 
+  // to reset Page
+  const refresh = () => window.location.reload();
+
   return (
     <>
       <div className="w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24 h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg ">
         <div className="mx-auto w-[300px]">
-          <section className="text-center">
-            <h2 className="text-2xl font-black">
-              {forecast.name}{" "}
-              <span className="font-thin">{forecast.country}</span>
-            </h2>
-            <h1 className="text-3xl font-extrabold">
-              <Degree temp={Math.round(today.main.temp)} />
-            </h1>
-            <p className="text-sm">
-              {today.weather[0].main} {today.weather[0].description}
-            </p>
-            <p className="text-sm">
-              <span className="font-bold">H:</span>
-              <Degree temp={Math.ceil(today.main.temp_max)} />{" "}
-              <span className="font-bold">L:</span>
-              <Degree temp={Math.floor(today.main.temp_min)} />
-            </p>
+          <section className=" flex items-center justify-between text-center">
+            <div>
+              <h2 className="text-2xl font-black">
+                {forecast.name}{" "}
+                <span className="font-thin">{forecast.country}</span>
+              </h2>
+              <h1 className="text-3xl font-extrabold">
+                <Degree temp={Math.round(today.main.temp)} />
+              </h1>
+              <p className="text-sm">
+                {today.weather[0].main} {today.weather[0].description}
+              </p>
+              <p className="text-sm">
+                <span className="font-bold">H:</span>
+                <Degree temp={Math.ceil(today.main.temp_max)} />{" "}
+                <span className="font-bold">L:</span>
+                <Degree temp={Math.floor(today.main.temp_min)} />
+              </p>
+            </div>
+            <div>
+              <button
+                className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                onClick={refresh}
+              >
+                Reset
+              </button>
+            </div>
           </section>
           <section className="flex overflow-x-scroll mt-4 pb-2 mb-5">
             {forecast.list.map((item, index) => (
